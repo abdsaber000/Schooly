173dd745ba1acc2f61ea03ea8b92f3062d3c9080
Anas Maged <ansmaged5@gmail.com>
2025-03-02 07:29:37 +0200

    Add reset password apis in auth controller

--- SchoolManagement.Api/Controllers/AuthenticationController.cs
+++ SchoolManagement.Api/Controllers/AuthenticationController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SchoolManagement.Application.Features.Authentication.Commands.Login;
 using SchoolManagement.Application.Features.Authentication.Commands.Register;
+using SchoolManagement.Application.Features.PasswordReset.Command.ForgetPassword;
+using SchoolManagement.Application.Features.PasswordReset.Command.ResetPassword;
+using SchoolManagement.Application.Features.PasswordReset.Command.VerifyCode;
 using SchoolManagement.Application.Services.ResponseService;
 using SchoolManagement.Domain.Entities;
 
@@ -14,14 +17,14 @@ public class AuthenticationController : ControllerBase
 {
     private readonly IMediator _mediator;
     private readonly IResponseService _responseService;
-    public AuthenticationController(IMediator mediator 
+    public AuthenticationController(IMediator mediator
                     , IResponseService responseService)
     {
         _mediator = mediator;
         _responseService = responseService;
     }
-    
-   
+
+
     [HttpPost]
     [Route("register")]
     public async Task<IActionResult> Register([FromBody] RegisterStudentCommand command)
@@ -32,6 +35,24 @@ public class AuthenticationController : ControllerBase
     [HttpPost]
     [Route("login")]
     public async Task<IActionResult> Login([FromBody] LoginUserCommand command)
+    {
+        return _responseService.CreateResponse(await _mediator.Send(command));
+    }
+
+    [HttpPost("forgot-password")]
+    public async Task<IActionResult> ForgotPassword([FromBody] ForgetPasswordCommand command)
+    {
+        return _responseService.CreateResponse(await _mediator.Send(command));
+    }
+
+    [HttpPost("verify-code")]
+    public async Task<IActionResult> VerifyCode([FromBody] VerifyCodeCommand command)
+    {
+        return _responseService.CreateResponse(await _mediator.Send(command));
+    }
+
+    [HttpPost("reset-password")]
+    public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordCommand command)
     {
         return _responseService.CreateResponse(await _mediator.Send(command));
     }
