675766ebbb4e1a1f1eee17c02b7a96e2e72253f5
Anas Maged <ansmaged5@gmail.com>
2025-03-02 07:43:57 +0200

    Add reset code entity in Db context

--- SchoolManagement.Infrastructure/DbContext/AppDbContext.cs
+++ SchoolManagement.Infrastructure/DbContext/AppDbContext.cs
@@ -15,46 +15,48 @@ public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         base.OnModelCreating(modelBuilder);
 
-         // change names of tables that identity make it 
-            modelBuilder.Entity<ApplicationUser>(e => e.ToTable("Users"));
-            modelBuilder.Entity<IdentityRole>(e => e.ToTable("Roles"));
-            modelBuilder.Entity<IdentityUserRole<string>>(e => e.ToTable("UserRoles"));
-            modelBuilder.Entity<IdentityUserClaim<string>>(e => e.ToTable("UserClaims"));
-            modelBuilder.Entity<IdentityUserLogin<string>>(e => e.ToTable("UserLogins"));
-            modelBuilder.Entity<IdentityRoleClaim<string>>(e => e.ToTable("RoleCliams"));
-            modelBuilder.Entity<IdentityUserToken<string>>(e => e.ToTable("UserTokens"));
-            
-            
-            // TPT (Table Per Type) Configuration: Student and Teacher will have their own tables
-            modelBuilder.Entity<Student>()
-                .ToTable("Student");  
-            
-            modelBuilder.Entity<Teacher>()
-                .ToTable("Teacher");
-            
-            modelBuilder.Entity<Post>()
-                .HasOne(p => p.Author)
-                .WithMany(auth => auth.Posts)
-                .HasForeignKey(p => p.AuthorId)
-                .OnDelete(DeleteBehavior.ClientCascade);
+        // change names of tables that identity make it 
+        modelBuilder.Entity<ApplicationUser>(e => e.ToTable("Users"));
+        modelBuilder.Entity<IdentityRole>(e => e.ToTable("Roles"));
+        modelBuilder.Entity<IdentityUserRole<string>>(e => e.ToTable("UserRoles"));
+        modelBuilder.Entity<IdentityUserClaim<string>>(e => e.ToTable("UserClaims"));
+        modelBuilder.Entity<IdentityUserLogin<string>>(e => e.ToTable("UserLogins"));
+        modelBuilder.Entity<IdentityRoleClaim<string>>(e => e.ToTable("RoleCliams"));
+        modelBuilder.Entity<IdentityUserToken<string>>(e => e.ToTable("UserTokens"));
 
-            modelBuilder.Entity<Comment>()
-                .HasOne(c => c.Author)
-                .WithMany(auth => auth.Comments)
-                .HasForeignKey(c => c.AuthorId)
-                .OnDelete(DeleteBehavior.ClientCascade); 
 
-            modelBuilder.Entity<Comment>()
-                .HasOne(c => c.Post)
-                .WithMany(p => p.Comments)
-                .HasForeignKey(c => c.PostId)
-                .OnDelete(DeleteBehavior.ClientCascade); 
+        // TPT (Table Per Type) Configuration: Student and Teacher will have their own tables
+        modelBuilder.Entity<Student>()
+            .ToTable("Student");
+
+        modelBuilder.Entity<Teacher>()
+            .ToTable("Teacher");
+
+        modelBuilder.Entity<Post>()
+            .HasOne(p => p.Author)
+            .WithMany(auth => auth.Posts)
+            .HasForeignKey(p => p.AuthorId)
+            .OnDelete(DeleteBehavior.ClientCascade);
+
+        modelBuilder.Entity<Comment>()
+            .HasOne(c => c.Author)
+            .WithMany(auth => auth.Comments)
+            .HasForeignKey(c => c.AuthorId)
+            .OnDelete(DeleteBehavior.ClientCascade);
+
+        modelBuilder.Entity<Comment>()
+            .HasOne(c => c.Post)
+            .WithMany(p => p.Comments)
+            .HasForeignKey(c => c.PostId)
+            .OnDelete(DeleteBehavior.ClientCascade);
     }
     public DbSet<Parent> Parents { get; set; }
-    public DbSet<UploadedFile> UploadedFiles {get; set;}
+    public DbSet<UploadedFile> UploadedFiles { get; set; }
     public DbSet<Lesson> Lessons { get; set; }
-    public DbSet<Comment> Comments {get; set;}
-    public DbSet<Post> Posts {get; set;}
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Post> Posts { get; set; }
     public DbSet<ClassRoom> ClassRooms { get; set; }
     public DbSet<HomeWork> HomeWorks { get; set; }
+    public DbSet<ResetCode> ResetCodes { get; set; }
+
 }
