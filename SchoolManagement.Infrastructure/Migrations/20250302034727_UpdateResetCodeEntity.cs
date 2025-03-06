2fac11d6d3a79e8b031cbac5bb2154cc045bb913
Anas Maged <ansmaged5@gmail.com>
2025-03-06 02:26:49 +0200

    Add database migrations

---
+++ SchoolManagement.Infrastructure/Migrations/20250302034727_UpdateResetCodeEntity.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateResetCodeEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "ResetCodes",
+                type: "nvarchar(6)",
+                maxLength: 6,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Code",
+                table: "ResetCodes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(6)",
+                oldMaxLength: 6);
+        }
+    }
+}
